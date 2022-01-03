811e0ca00a86acbe8a5d9f60f83a014ab0546178
danjhkim <danjhkim@gmail.com>
2022-01-03 01:18:46 -0500

    Updates

---
+++ precache-manifest.69ba9717cd28e091b39d942213056d9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b98ed1e793ba778c5b0e",
+    "url": "/PWA-example-robofriends/static/css/main.46b169de.chunk.css"
+  },
+  {
+    "revision": "b98ed1e793ba778c5b0e",
+    "url": "/PWA-example-robofriends/static/js/main.b98ed1e7.chunk.js"
+  },
+  {
+    "revision": "496f2dcacd76b5297a52",
+    "url": "/PWA-example-robofriends/static/css/1.b9ad712d.chunk.css"
+  },
+  {
+    "revision": "496f2dcacd76b5297a52",
+    "url": "/PWA-example-robofriends/static/js/1.496f2dca.chunk.js"
+  },
+  {
+    "revision": "0c79e8c0e9067ee96a2b",
+    "url": "/PWA-example-robofriends/static/js/runtime~main.0c79e8c0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/PWA-example-robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "9c456230b886ec6d929ebe1dd5ce03f3",
+    "url": "/PWA-example-robofriends/index.html"
+  }
+];
